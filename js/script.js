eebee74fc4ad41b1f34f1b26b1397b08498f20d6
lindam <linda.minotti2@gmail.com>
2020-06-29 16:11:33 +0200

    Chiamat Ajax

--- js/script.js
+++ js/script.js
@@ -1,39 +1,49 @@
 $(document).ready(function(){
-  // Setto primo giorno dell'anno di interesse
-  var date = moment("2018-01-01");
-
-  // Calcolo quanti giorni ha il mese corrente
-  var totDayMonth = date.daysInMonth();
-  // Stampo i giorni del mese corrente
-  // Uso un template con Handelbars per scrivere la lista dei giorni
-  var source = document.getElementById("calendar-template").innerHTML;
-  var template = Handlebars.compile(source);
-  // Ciclo in base al numero dei giorni del mese corrente
-  for (var i = 0; i < totDayMonth; i++) {
-    console.log(date);
-    var day = (date.format('D'));
-    var month = (date.format('MMMM'));
-    var dateToStamp = {day: day, month: month};
-    var html = template(dateToStamp);
-    $('.calendar').append(html);
-    date.add(1,'days');
-  };
 
   // Chiamata Ajax per vedere le festività del mese corrente
-  // $.ajax({
-  //   url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
-  //   method: 'GET',
-  //   success: function(data) {
-  //   var festivity = data.response;
-  //   console.log(festivity);
+  $.ajax({
+    url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
+    method: 'GET',
+    success: function(data) {
 
-  //   },
-  //   error: function() {
-  //     alert('errore per reperire info su festività');
-  //   }
-  // });
+      // Setto primo giorno dell'anno di interesse
+      var startDate = moment("2018-01-01");
+
+      // Calcolo quanti giorni ha il mese corrente
+      var totDayMonth = startDate.daysInMonth();
+      // Stampo i giorni del mese corrente
+      // Uso un template con Handelbars per scrivere la lista dei giorni
+      var source = document.getElementById("calendar-template").innerHTML;
+      var template = Handlebars.compile(source);
+
+    // Vedere le festività del mese corrente
+    var festivityCalendar = data.response;
+    for (var i = 0; i < festivityCalendar.length; i++) {
+      var singleFestivity = festivityCalendar[i].date;
+
+      // Ciclo in base al numero dei giorni del mese corrente
+      for (var i = 0; i < totDayMonth; i++) {
+        var currentDate = startDate.format('YYYY-MM-DD');
+        var day = (startDate.format('D'));
+        var month = (startDate.format('MMMM'));
+
+        if (currentDate === singleFestivity) {
+          var classe = 'festivity';
+          var dateToStamp = {class: classe, day: day, month: month};
+        } else {
+          var dateToStamp = {day: day, month: month};
+        }
+
+        var html = template(dateToStamp);
+        $('.calendar').append(html);
+        startDate.add(1,'days');
+      };
+    }
+
+    },
+    error: function() {
+      alert('errore per reperire info su festività');
+    }
+  });
   // Fine chiamata ajax
-
-
-
 });
