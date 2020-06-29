a2d65cd25d4ddd4ad03b6d5f187e951be63a21fc
lindam <linda.minotti2@gmail.com>
2020-06-29 16:29:51 +0200

    Errore nel ciclo.

--- js/script.js
+++ js/script.js
@@ -1,49 +1,41 @@
 $(document).ready(function(){
+  // Setto primo giorno dell'anno di interesse
+  var startDate = moment("2018-01-01");
+
+  // Calcolo quanti giorni ha il mese corrente
+  var totDayMonth = startDate.daysInMonth();
+  // Stampo i giorni del mese corrente
+  // Uso un template con Handelbars per scrivere la lista dei giorni
+  var source = document.getElementById("calendar-template").innerHTML;
+  var template = Handlebars.compile(source);
+
+  // Ciclo in base al numero dei giorni del mese corrente
+  for (var i = 0; i < totDayMonth; i++) {
+    var newDay = startDate.add(i,'days');
+    console.log(newDay);
+    var day = (newDay.format('D'));
+    var month = (newDay.format('MMMM'));
+    var dateToStamp = {day: day, month: month}
+    var html = template(dateToStamp);
+    $('.calendar').append(html);
+  };
 
   // Chiamata Ajax per vedere le festività del mese corrente
-  $.ajax({
-    url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
-    method: 'GET',
-    success: function(data) {
-
-      // Setto primo giorno dell'anno di interesse
-      var startDate = moment("2018-01-01");
-
-      // Calcolo quanti giorni ha il mese corrente
-      var totDayMonth = startDate.daysInMonth();
-      // Stampo i giorni del mese corrente
-      // Uso un template con Handelbars per scrivere la lista dei giorni
-      var source = document.getElementById("calendar-template").innerHTML;
-      var template = Handlebars.compile(source);
-
-    // Vedere le festività del mese corrente
-    var festivityCalendar = data.response;
-    for (var i = 0; i < festivityCalendar.length; i++) {
-      var singleFestivity = festivityCalendar[i].date;
-
-      // Ciclo in base al numero dei giorni del mese corrente
-      for (var i = 0; i < totDayMonth; i++) {
-        var currentDate = startDate.format('YYYY-MM-DD');
-        var day = (startDate.format('D'));
-        var month = (startDate.format('MMMM'));
-
-        if (currentDate === singleFestivity) {
-          var classe = 'festivity';
-          var dateToStamp = {class: classe, day: day, month: month};
-        } else {
-          var dateToStamp = {day: day, month: month};
-        }
-
-        var html = template(dateToStamp);
-        $('.calendar').append(html);
-        startDate.add(1,'days');
-      };
-    }
-
-    },
-    error: function() {
-      alert('errore per reperire info su festività');
-    }
-  });
+  // $.ajax({
+  //   url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
+  //   method: 'GET',
+  //   success: function(data) {
+  //
+  //     // Vedere le festività del mese corrente
+  //     var festivityCalendar = data.response;
+  //     for (var i = 0; i < festivityCalendar.length; i++) {
+  //       var singleFestivity = festivityCalendar[i].date;
+  //       console.log(singleFestivity);
+  //     };
+  //   },
+  //   error: function() {
+  //     alert('errore per reperire info su festività');
+  //   }
+  // });
   // Fine chiamata ajax
 });
