1cc58e85b4387b17fcf78749150db524c46855ff
lindam <linda.minotti2@gmail.com>
2020-06-29 18:20:05 +0200

    Creato calendario dinamico, con le festività in rosso per gennaio.

--- js/script.js
+++ js/script.js
@@ -9,11 +9,12 @@ $(document).ready(function(){
   var source = document.getElementById("calendar-template").innerHTML;
   var template = Handlebars.compile(source);
 
-  // Ciclo in base al numero dei giorni del mese corrente
+  // Ciclo in base al numero dei giorni del mese corrente per stampare
   for (var i = 0; i < totDayMonth; i++) {
     var newDay = moment(startDate).add(i,'days');
     var day = (newDay.format('D'));
     var month = (newDay.format('MMMM'));
+    // creo un attributo per salvare la data ed usarla per controllare se è festività
     var totalDate = newDay.format('YYYY-MM-DD');
     var dateToStamp = {day: day, month: month, date: totalDate};
     var html = template(dateToStamp);
@@ -30,14 +31,10 @@ $(document).ready(function(){
         var festivityCalendar = data.response;
         for (var i = 0; i < festivityCalendar.length; i++) {
           var singleFestivity = festivityCalendar[i].date;
-          console.log(singleFestivity);
 
-          for (var j = 0; j < $('.calendar').length; j++) {
-            if (singleFestivity === $('.day-calendar').attr('data-date')) {
-              $('.day-calendar').addClass('festivity');
-            }
-          }
-
+          // Chi ha l'attributo data-date uguale alla festività
+          // prende la classe per evidenziare la festività
+          $(".day-calendar[data-date='" + singleFestivity + "']").addClass("festivity");
         };
       },
       error: function() {
