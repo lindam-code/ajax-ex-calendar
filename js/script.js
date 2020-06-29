ef17f4b60d7195e7832151e97711f19aca5441b9
lindam <linda.minotti2@gmail.com>
2020-06-29 17:41:58 +0200

    Festività in rosso, non corretto.

--- js/script.js
+++ js/script.js
@@ -14,9 +14,35 @@ $(document).ready(function(){
     var newDay = moment(startDate).add(i,'days');
     var day = (newDay.format('D'));
     var month = (newDay.format('MMMM'));
-    var dateToStamp = {day: day, month: month}
+    var totalDate = newDay.format('YYYY-MM-DD');
+    var dateToStamp = {day: day, month: month, date: totalDate};
     var html = template(dateToStamp);
     $('.calendar').append(html);
   };
 
+  // Chiamata Ajax per vedere le festività del mese corrente
+    $.ajax({
+      url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
+      method: 'GET',
+      success: function(data) {
+
+        // Vedere le festività del mese corrente
+        var festivityCalendar = data.response;
+        for (var i = 0; i < festivityCalendar.length; i++) {
+          var singleFestivity = festivityCalendar[i].date;
+          console.log(singleFestivity);
+
+         if (singleFestivity === $('.day-calendar').attr('data-date')) {
+          $('.day-calendar').addClass('festivity');
+
+
+         }
+
+        };
+      },
+      error: function() {
+        alert('errore per reperire info su festività');
+      }
+    });
+  // Fine chiamata ajax
 });
